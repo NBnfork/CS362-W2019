fd9e8037d24914e818c48294862c3ebeabf7934f
Noah <buchenn@oregonstate.edu>
2019-02-25 00:28:14 -0500

    just starting random tests

--- projects/buchenn/dominion/myTestSuite.c
+++ projects/buchenn/dominion/myTestSuite.c
@@ -16,30 +16,65 @@
 
 #include <memory.h>
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 #include "myTestSuite.h"
 #include "dominion.h"
 #include "dominion_helpers.h"
 
-/***************************************************************************
- * Name: insertSort
- * Description: This function will use the insertSort algorithm to sort an
- * array of integers of give size.
-***************************************************************************/
-void insertSort(int* theArray, int size) {
+int successfulTests = 0;
 
-	//sort the array
-	for (int i = 0; i < size; ++i) {
-		int temp = theArray[i];
-		int j = i-1;
-		while (temp < theArray[j] && j >= 0){
-			theArray[j + 1] = theArray [j];
-			j--;
+void initializeRandomGame(int numPlayers,  int kingdomCards[10], int randomSeed,
+						 struct gameState *state){
+
+
+}
+void randomTestDriver (int numPlayers, int testCase){
+
+	struct gameState unmutable;
+	struct gameState mutable;
+	int testResult;
+
+	//need more flexibility in card declaration
+	int k[10] = {adventurer, gardens, embargo, village, minion, salvager, cutpurse,
+				 sea_hag, tribute, smithy};
+	/*
+	//set last 7 kingdom cards at random
+	int k[10] = {salvager, smithy, adventurer};
+	for (int i = 3; i < 10; ++i) {
+		k[i] =
+
+	}*/
+	//produce random seed
+	int seed = rand();
+
+	//Intialize mutable gamestate
+	memset(&mutable, 'z', sizeof(struct gameState));
+	initializeRandomGame(numPlayers, k, seed, &mutable);
+
+	//copy to unmutable gamestate
+	memcpy (&unmutable, &mutable, sizeof(struct gameState));
+	printf("Testing Initializing and memcpy\n");
+	testResult = memcmp(&mutable, &unmutable, sizeof(struct gameState));
+	printf("Memcmp results = %d ", testResult);
+	asserttruerandom((testResult == 0));
+
+	switch(testCase) {
+		case SMITHY: {
+			testResult = playCardTestSmithy(numPlayers, &mutable, &unmutable);
+			if (!testResult) {
+				printf("**TEST SUITE ERROR: Smithy**\n");
+			}
 		}
-		theArray[j+1] = temp;
-
+			break;
+		case ADVENTURER: {
+			testResult = playCardTestAdventurer(numPlayers, &mutable, &unmutable);
+			if (!testResult) {
+				printf("**TEST SUITE ERROR: Adventurer**\n");
+			}
+		}
+			break;
 	}
-
-
 }
 
 void testDriverDominion(int numPlayers, int testCase){
@@ -66,7 +101,7 @@ void testDriverDominion(int numPlayers, int testCase){
 	printf("Memcmp results = %d ", testResult);
 	asserttrue((testResult == 0));
 
-	//print gamestate guide
+	/*//print gamestate guide
 	printf ("Rough guide to locations in structure:\n");
 	printf ("0: numPlayers\n");
 	printf ("%ld: supplyCount[0]\n", ((long)&(mutable.supplyCount[0]))-((long)&mutable));
@@ -99,7 +134,7 @@ void testDriverDominion(int numPlayers, int testCase){
 		} else {
 			printf ("Bytes %d-%d uninitialized.\n", start, i-1);
 		}
-	}
+	}*/
 	switch(testCase) {
 		case SHUFFLE: {
 			//test shuffle
@@ -111,60 +146,7 @@ void testDriverDominion(int numPlayers, int testCase){
 			}
 			printf("**TESTING: shuffle**\n");
 
-			//int result = 1;
-
-			//test normal coverage use deck of player 2 because player 1 draws random cards during intialize
-			//result = shuffle(1, &mutable);
-			//asserttrue(result == 0);
-			//test gamestate
-			//qsort ((void*)(mutable.deck[1]), mutable.deckCount[1], sizeof(int), compare);
-			//qsort ((void*)(unmutable.deck[1]), unmutable.deckCount[1], sizeof(int), compare);
-			//result = memcmp(&mutable, &unmutable, sizeof(struct gameState));
-			//printf("Memcmp results = %d", result);
-			//asserttrue(result == 0);
-
-			//test shuffle changes order (use larger enough deck to increase assurance)
-			//add 20 more card to two decks
-			//for (int m = 0; m < 2; ++m) {
-			//	for (int j = mutable.deckCount[m]; j < 20; j++) {
-			//		mutable.deck[m][j] = estate;
-			//		mutable.deckCount[m]++;
-			//	}
-			//	for (int k = 20; k < 30; k++) {
-
-			//		mutable.deck[m][k] = copper;
-			//		mutable.deckCount[m]++;
-			//	}
-			//}
-			//asserttrue((shuffle(0, &mutable) == 0));
-
-			//test shuffle does not effect deckCount
-			//asserttrue((mutable.deckCount[0] == 30));
-
-			//test deck cards are unchanged
-			//sort decks and compare
-			//qsort ((void*)(mutable.deck[0]), mutable.deckCount[0], sizeof(int), compare);
-			//qsort ((void*)(mutable.deck[1]), mutable.deckCount[1], sizeof(int), compare);
-			//compare decks
-			//for (int k = 0; k < 30; ++k) {
-				//different order passes
-			//	if (mutable.deck[0][k] == mutable.deck[1][k]) {
-			//		result = 1;
-			//	}
-			//	else {
-			//		result = -1;
-			//		break;
-			//	}
-			//}
-			//asserttrue((result == 1));
-
-			//test for empty deck conditional, set deck count to 0,
-			//mutable.deckCount[0] = 0;
-			//result = shuffle(0, &mutable);
-			//asserttrue((result == -1));
-
-
-		}
+			}
 			break;
 		case SMITHY: {
 			testResult = playCardTestSmithy(numPlayers, &mutable, &unmutable);
@@ -189,6 +171,8 @@ void testDriverDominion(int numPlayers, int testCase){
 
 int main(){
 
+	//seed rand
+	srand(time);
 	//Unit testing
 
 	//TODO refactor into reading from function list a.k.a practice function pointers in C
--- projects/buchenn/dominion/myTestSuite.h
+++ projects/buchenn/dominion/myTestSuite.h
@@ -8,7 +8,9 @@
 #include "dominion_helpers.h"
 
 //macro created by Jonah Siekmann posted on 9 Feb. 2019 https://piazza.com/class/jpu18p346423vs?cid=159
+//edited for random
 #define asserttrue(bool)if(bool > 0) printf("TEST SUCCESSFULLY COMPLETED.\n"); else printf("TEST FAILED: '" #bool  "' on line %d.\n", __LINE__);
+#define asserttruerandom(bool)if(bool > 0) successfulTests++; else printf("TEST FAILED ON LINE: %d.\n", __LINE__);
 
 enum testCase { SHUFFLE = 1,
 		        DRAWCARD,
@@ -18,8 +20,8 @@ enum testCase { SHUFFLE = 1,
 			    ADVENTURER,
 			    EMBARGO,
 			    SALVAGER };
-
-void insertSort(int* theArray, int size);
+void initializeRandomGame(int, int*, int, struct gameState*);
+void randomTestDriver (int numPlayers, int testCase);
 void testDriverDominion(int numPlayers, int testCase);
 int myShuffleTest(int numPlayers, struct gameState* mutable, struct gameState* unmutable);
 int myDrawCardTest(int numPlayers, struct gameState* mutable, struct gameState* unmutable);

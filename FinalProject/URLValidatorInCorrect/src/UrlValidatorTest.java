e2facdd2443d76b7340cb6b72265c24fdb13fc92
Noah <buchenn@oregonstate.edu>
2019-03-11 17:55:02 -0400

    goodURL randomTester in progress

--- FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
@@ -25,11 +25,68 @@ public class UrlValidatorTest extends TestCase {
 
 
     public void testYourFirstPartition() {
-        //You can use this function to implement your First Partition testin
+    //first partition will test random good URLs made from some arrays of different parts
+        //make good schemes and good Authrorities
+        String[] goodSchemes = new String[]{"http://", "ftp://", "https://"};
+        String[] goodAuthorities = new String[]{"www.google.com",
+                                                "go.com",
+                                                "go.au",
+                                                "0.0.0.0",
+                                                "255.255.255.255",
+                                                "255.com",
+                                                "go.cn"};
 
-        System.out.print(getrandomString(0, 200, 10));
+        //ports number will be generated randomly
+        String portColon = ":";
+
+        //goodpaths just need slashes and randomstrings
+        String[] slash = new String[] {"", "/", "/"};
+
+        //goodQueries = ? string = string ; etc.
+        char qMark = '?';
+        char qEqual = '=';
+
+        //good URL building algorithm
+        String goodURL;
+        //get random scheme and append
+        int schemeChoice = (int)(Math.random() * 2);
+        goodURL = goodSchemes[schemeChoice];
+        //get good Authority and append
+        int authoritityChoice = (int)(Math.random() * 6);
+        goodURL += goodAuthorities[authoritityChoice];
+        //add port?
+        int addPortBool = (int)(Math.random() * 1);
+        if(addPortBool > 0){
+            //add colon and random port num
+            goodURL += portColon;
+            int goodPortInt = ((int)(Math.random() * 65535));
+            //cast portnum to string
+            String goodPortStr = Integer.toString(goodPortInt);
+            goodURL += goodPortStr;
+        }
+
+        //add path with random number of interations MAX = 4
+        int addPathIterator = (int)(Math.random() * 4);
+        while(addPathIterator > 0) {
+            //add single slash
+            goodURL += slash;
+            //add string of random length
+            int randomStringLength = (int) (Math.random() * 12);
+            //characters a-z
+            String goodPathString = getRandomString(97, 122, randomStringLength);
+            goodURL += goodPathString;
+            addPathIterator--;
+        }
+        //add a trailing slash(s)
+        int trailingSlashType = (int)(Math.random() * 2); //0="", 1 = /, 2 = //
+        goodURL += slash[trailingSlashType];
+
+            //add query with random number of interations
+        int randomStringLength = (int) (Math.random() * 12);
+        //characters a-z
+        String randomQueryString = getRandomString(97, 122, randomStringLength);
+            System.out.println(goodURL);
     }
-
     public void testYourSecondPartition() {
         //You can use this function to implement your Second Partition testing
 
@@ -46,7 +103,7 @@ public class UrlValidatorTest extends TestCase {
     https://www.baeldung.com/java-random-string
     @param: leftLimit and rightLimit is ASCII code for char to used, length is length of string returned
     */
-    private String getrandomString(int leftLimit, int righLimit, int length) {
+    private String getRandomString(int leftLimit, int righLimit, int length) {
 
         int lLimit = leftLimit; // letter 'a'
         int rLimit = righLimit; // letter 'z'
@@ -64,6 +121,11 @@ public class UrlValidatorTest extends TestCase {
     }
 
     public static void main(String[] args){
+
+        String testTest = "TestName";
+        UrlValidatorTest test = new UrlValidatorTest(testTest);
+
+        test.testYourFirstPartition();
     }
 
 }
